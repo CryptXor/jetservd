a5d86834004289dce843585f9de912456fbfcff4
CryptXor <laclouph@nonkly.com>
2016-01-24 10:21:23 -0600

    Daemon Skeleton

--- main.cpp
+++ main.cpp
@@ -8,85 +8,86 @@
 #include <syslog.h>
 #include "mongoose.h"
 
-using namespace std;
+#define DAEMON_NAME "jetservd"
+#define DAEMON_PORT 15243
 
-bool daemon_running = false;
-static const char *daemon_bindport = "1234";
 struct mg_mgr mgr;
 struct mg_connection *nc;
-static pid_t process_id, session_id;
+static int s_sig_num = 0;
 
-static void daemon_handler(struct mg_connection *nc, int ev, void *p) {
-    if (ev == MG_EV_HTTP_REQUEST) {
-        mg_printf(nc, "HTTP/1.0 200 OK\r\n"
-                "Content-Length: 2\r\n"
-                "Content-Type: text/html\r\n\r\nCheck the docs!");
-    }
+using namespace std;
 
-}
 
-static void print_menu(bool status) {
-    printf("Select an option from below:\n");
-    if (status) {
-        printf("\t[1] Start emulator\n");
-        printf("\t[2] Install emulator (NOT IMPLEMENTED)\n");
-    } else {
-        printf("\t[1] Stop emulator\n");
-        printf("\t[2] Uninstall emulator (NOT IMPLEMENTED)\n");
-    }
-    printf("\t[3] Exit emulator\n");
-}
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
+    struct http_message *hm = (struct http_message *) ev_data;
+    string s_method_post = "POST";
+    switch (ev) {
+        case MG_EV_HTTP_REQUEST:
+            if (mg_vcmp(&hm->method, s_method_post.c_str())) {
+                mg_printf(nc, "HTTP/1.0 200 OK\r\n"
+                        "Content-Length: 2\r\n"
+                        "Content-Type: text/html\r\n\r\nNo");
+            }
 
-static void daemon_manager(bool status) {
-    static bool daemon_loop = true;
-    if (status) {
-        daemon_running = true;
-        process_id = fork();
-        if (process_id < 0) {
-            syslog(LOG_CRIT, "Failed to create child process");
-            exit(EXIT_FAILURE);
-        }
-        if (process_id > 0) {
-            exit(EXIT_SUCCESS);
-        }
-        session_id = setsid();
-        if (session_id < 0) {
-            syslog(LOG_CRIT, "Failed to create Session ID");
-            exit(EXIT_FAILURE);
-        }
-        mg_mgr_init(&mgr, NULL);
-        nc = mg_bind(&mgr, daemon_bindport, daemon_handler);
-        syslog(LOG_INFO, "Daemon has been bound to port %s", daemon_bindport);
-        while (daemon_loop) {
-            mg_mgr_poll(&mgr, 1000);
-        }
-        exit(EXIT_SUCCESS);
-    } else {
-        daemon_running = false;
+            break;
+        default:
+            break;
     }
 }
 
-int main() {
-    bool bLoop = true;
-    string string_input;
-    openlog("jetservd", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-    do {
-        print_menu(daemon_running);
-        printf("Selection: ");
-        getline(cin, string_input);
-        switch (atoi(string_input.c_str())) {
-            case 1:
-                daemon_manager(daemon_running);
-                break;
-            case 2:
-            case 3:
-                bLoop = false;
-                break;
-            default:
-                printf("\nInvalid input\n");
-        }
-    } while (bLoop);
-    printf("\nShutting down emulator");
+
+void jetservdaemon() {
+    syslog(LOG_NOTICE, "jetservd successfully started");
+
+    /* Open listening socket */
+    mg_mgr_init(&mgr, NULL);
+    nc = mg_bind(&mgr, (const char *) DAEMON_PORT, ev_handler);
+    mg_set_protocol_http_websocket(nc);
+
+    /* Run event loop until signal is received */
+    while (s_sig_num == 0) {
+        mg_mgr_poll(&mgr, 1000);
+    }
+
+    /* Cleanup */
     mg_mgr_free(&mgr);
-    return (EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+
+    //Set our Logging Mask and open the Log
+    openlog(DAEMON_NAME, LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
+
+    syslog(LOG_INFO, "Starting jetservd");
+
+    pid_t pid, sid;
+
+    //Fork the Parent Process
+    pid = fork();
+
+    if (pid < 0) { exit(EXIT_FAILURE); }
+
+    //We got a good pid, Close the Parent Process
+    if (pid > 0) { exit(EXIT_SUCCESS); }
+
+    //Change File Mask
+    umask(0);
+
+    //Create a new Signature Id for our child
+    sid = setsid();
+    if (sid < 0) { exit(EXIT_FAILURE); }
+
+    //Change Directory
+    //If we cant find the directory we exit with failure.
+    if ((chdir("/")) < 0) { exit(EXIT_FAILURE); }
+
+    //Close Standard File Descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    jetservdaemon();
+
+    //Close the log
+    closelog();
 }
