2e4339b5dd4124363d312c025c61ed2ac6f317bc
CryptXor <laclouph@nonkly.com>
2016-01-24 07:55:48 -0600

    Complicated mess which has been committed for a record. Clean recode going forward.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <syslog.h>
 #include "mongoose.h"
 
 using namespace std;
@@ -7,6 +14,7 @@ bool daemon_running = false;
 static const char *daemon_bindport = "1234";
 struct mg_mgr mgr;
 struct mg_connection *nc;
+static pid_t process_id, session_id;
 
 static void daemon_handler(struct mg_connection *nc, int ev, void *p) {
     if (ev == MG_EV_HTTP_REQUEST) {
@@ -14,6 +22,7 @@ static void daemon_handler(struct mg_connection *nc, int ev, void *p) {
                 "Content-Length: 2\r\n"
                 "Content-Type: text/html\r\n\r\nCheck the docs!");
     }
+
 }
 
 static void print_menu(bool status) {
@@ -28,23 +37,39 @@ static void print_menu(bool status) {
     printf("\t[3] Exit emulator\n");
 }
 
-static bool daemon_manager(bool status) {
-    mg_mgr_init(&mgr, NULL);
-    nc = mg_bind(&mgr, daemon_bindport, daemon_handler);
-    printf("Starting web server on port %s\n", daemon_bindport);
+static void daemon_manager(bool status) {
+    static bool daemon_loop = true;
     if (status) {
         daemon_running = true;
-        mg_mgr_poll(&mgr, 1000);
-        return true;
+        process_id = fork();
+        if (process_id < 0) {
+            syslog(LOG_CRIT, "Failed to create child process");
+            exit(EXIT_FAILURE);
+        }
+        if (process_id > 0) {
+            exit(EXIT_SUCCESS);
+        }
+        session_id = setsid();
+        if (session_id < 0) {
+            syslog(LOG_CRIT, "Failed to create Session ID");
+            exit(EXIT_FAILURE);
+        }
+        mg_mgr_init(&mgr, NULL);
+        nc = mg_bind(&mgr, daemon_bindport, daemon_handler);
+        syslog(LOG_INFO, "Daemon has been bound to port %s", daemon_bindport);
+        while (daemon_loop) {
+            mg_mgr_poll(&mgr, 1000);
+        }
+        exit(EXIT_SUCCESS);
     } else {
         daemon_running = false;
-        return false;
     }
 }
 
 int main() {
     bool bLoop = true;
     string string_input;
+    openlog("jetservd", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
     do {
         print_menu(daemon_running);
         printf("Selection: ");
@@ -54,6 +79,7 @@ int main() {
                 daemon_manager(daemon_running);
                 break;
             case 2:
+            case 3:
                 bLoop = false;
                 break;
             default:
@@ -62,5 +88,5 @@ int main() {
     } while (bLoop);
     printf("\nShutting down emulator");
     mg_mgr_free(&mgr);
-    return 0;
+    return (EXIT_SUCCESS);
 }
